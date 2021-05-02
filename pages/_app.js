68c20b9a778041dee7e50bf1dbaa5e8a214fe991
Josselin <julien.josselin75@hotmail.fr>
2021-05-02 15:20:55 +0200

    global: switch language from navbar affects home

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
+import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [language, setLanguage] = useState("fr");
+  const changeLanguage = () => {
+    language === "fr" ? setLanguage("en") : setLanguage("fr");
+  };
+
+  useEffect(() => {
+    const storageLanguage = localStorage.getItem("lang");
+    setLanguage(storageLanguage);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("lang", language);
+  }, [language]);
+
+  return (
+    <>
+      <Navbar changeLanguage={changeLanguage} language={language} />
+      <Component {...pageProps} language={language} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,22 @@
+import React from "react";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.scss";
 
-export default function Home() {
-  const [language, setLanguage] = useState("fr");
-
-  useEffect(() => {
-    const storageLanguage = localStorage.getItem("lang");
-    setLanguage(storageLanguage);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("lang", language);
-  }, [language]);
-
+export default function Home({ language }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Josselin Julien | Développeur</title>
+        <title>Josselin Julien | Développeur Web Fullstack</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Josselin Julien</h1>
-
         <div className={styles.description}>
           <p>Développeur web fullstack</p>
           <p>Ruby on Rails | React.js | Next.js | Node.js</p>
-          <button
-            onClick={() => {
-              language === "fr"
-                ? setLanguage("en")
-                : setLanguage("fr");
-            }}
-          >
-            change language
-          </button>
-          <span>{language}</span>
+          <p>{language}</p>
         </div>
       </main>
     </div>
