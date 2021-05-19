7005f309e2aa1cdca8ce6491798dea788f9e0465
Josselin <julien.josselin75@hotmail.fr>
2021-05-19 09:58:36 +0200

    projects: add description p tag in ProjectDisplay

--- components/ProjectDisplay.js
+++ components/ProjectDisplay.js
@@ -1,11 +1,12 @@
 export default function ProjectDisplay({ projectData }) {
-  const { name, id, title, date } = projectData;
+  const { name, id, title, date, description } = projectData;
 
   return (
     <div>
       <div>
         <p>{title}</p>
         <p>{date}</p>
+        <p>{description}</p>
         <br />
       </div>
     </div>
