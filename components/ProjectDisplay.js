43060268d01aaf252522ecb6b09761e5f197af6b
Josselin <julien.josselin75@hotmail.fr>
2021-05-19 09:34:20 +0200

    projects: update ProjectDisplay component

--- components/ProjectDisplay.js
+++ components/ProjectDisplay.js
@@ -1,8 +1,12 @@
 export default function ProjectDisplay({ projectData }) {
+  const { name, id, title, date } = projectData;
+
   return (
     <div>
       <div>
-        <p>{projectData.name}</p>
+        <p>{title}</p>
+        <p>{date}</p>
+        <br />
       </div>
     </div>
   );
