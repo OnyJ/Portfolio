13681f4ecfee05edf7c301387a612794eb9e0f11
Josselin <julien.josselin75@hotmail.fr>
2021-05-21 10:47:29 +0200

    projects: modal text renamed into description

--- components/Project/ProjectModal.js
+++ components/Project/ProjectModal.js
@@ -1,8 +1,8 @@
-export default function ProjectModal({ text }) {
+export default function ProjectModal({ description }) {
   return (
     <div styles={{ backgroundColor: "yellow" }}>
       <h2>ProjectModal</h2>
-      {text}
+      {description}
     </div>
   );
 }
